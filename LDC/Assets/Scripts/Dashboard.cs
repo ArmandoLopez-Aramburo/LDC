5bbc9003027f942a4dc4f2fa1ff89f47bd8673f5
ArmandoLopez-Aramburo <armando.lopez-aramburo@okstate.edu>
2021-04-09 15:04:42 -0500

    Inventory Layout
    Added in the first stages of an Inventory Panel and removed the Main Secondary Camera.

---
+++ LDC/Assets/Scripts/Dashboard.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dashboard : MonoBehaviour
+{
+    [SerializeField] GameObject Inventory;
+
+    bool InventoryStatus = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Tab))
+        {
+            InventoryStatus = !InventoryStatus;
+            Inventory.SetActive(InventoryStatus);
+        }
+    }
+}
