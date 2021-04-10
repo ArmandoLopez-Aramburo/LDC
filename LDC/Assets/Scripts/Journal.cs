9a210b65ebe3d3a80a282ccd1310b67689fffad1
ArmandoLopez-Aramburo <armando.lopez-aramburo@okstate.edu>
2021-04-09 21:21:34 -0500

    Added TMP and Journal Script
    TMP = TextMeshPro, more HD text.
    
    Journal Script will contain the entries for quests, player info, and player history.

---
+++ LDC/Assets/Scripts/Journal.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Journal : MonoBehaviour
+{
+    [Header("Tabs")]
+    [SerializeField] GameObject QuestPanel;
+    [SerializeField] GameObject PlayerPanel;
+    [SerializeField] GameObject HistoryPanel;
+
+    public void Quest()
+    {
+        SetPanels(QuestPanel);
+    }
+
+    public void PlayerInfo()
+    {
+        SetPanels(PlayerPanel);
+    }
+
+    public void History()
+    {
+        SetPanels(HistoryPanel);
+    }
+
+    void SetPanels(GameObject panel)
+    {
+        panel.SetActive(true);
+        if (panel != QuestPanel) QuestPanel.SetActive(false);
+        if (panel != PlayerPanel) PlayerPanel.SetActive(false);
+        if (panel != HistoryPanel) HistoryPanel.SetActive(false);
+    }
+}
