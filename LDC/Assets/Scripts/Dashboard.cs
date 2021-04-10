f0d01040fe835520c55888cdf63115a97a6a9b5d
ArmandoLopez-Aramburo <armando.lopez-aramburo@okstate.edu>
2021-04-09 20:21:03 -0500

    Dashboard Update
    Updated Dashboard

--- LDC/Assets/Scripts/Dashboard.cs
+++ LDC/Assets/Scripts/Dashboard.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Dashboard : MonoBehaviour
 {
+    [Header("Panels")]
     [SerializeField] GameObject InventoryPanel;
     [SerializeField] GameObject JournalPanel;
     [SerializeField] GameObject MapPanel;
@@ -12,23 +13,30 @@ public class Dashboard : MonoBehaviour
     bool JournalStatus = false;
     bool MapStatus = false;
 
+    bool Status = false;
+
     // Update is called once per frame
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.Tab))
         {
-            InventoryStatus = !InventoryStatus;
-            InventoryPanel.SetActive(InventoryStatus);
+            InventoryStatus = PanelState(InventoryPanel, InventoryStatus);
         }
         if (Input.GetKeyDown(KeyCode.J))
         {
-            JournalStatus = !JournalStatus;
-            JournalPanel.SetActive(JournalStatus);
+
+            JournalStatus = PanelState(JournalPanel, JournalStatus);
         }
         if (Input.GetKeyDown(KeyCode.M))
         {
-            MapStatus = !MapStatus;
-            MapPanel.SetActive(MapStatus);
+            MapStatus = PanelState(MapPanel, MapStatus);
         }
+    }
+
+    bool PanelState(GameObject Panel, bool Status)
+    {
+        Status = !Status;
+        Panel.SetActive(Status);
+        return Status;
     }
 }
