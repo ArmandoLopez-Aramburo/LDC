68e19ea82340b2ed377409255eda995cb298e1b5
ArmandoLopez-Aramburo <armando.lopez-aramburo@okstate.edu>
2021-04-15 14:19:41 -0500

    Start Menu has been added
    A start menu has been added, given the player to start the game, tweak a few settings, or exit the game.

---
+++ LDC/Assets/Scripts/Menu.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    [Header("Menu's")]
+    public GameObject MainMenu;
+    public GameObject SettingsMenu;
+    public GameObject ExitMenu;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Settings(bool state)
+    {
+        MenuSelect(SettingsMenu, state);
+    }
+
+    public void ExitGame(bool state)
+    {
+        MenuSelect(ExitMenu, state);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void MenuSelect(GameObject panel, bool state)
+    {
+        if (state) panel.SetActive(true);
+        else panel.SetActive(false);
+    }
+}
